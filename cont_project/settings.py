c4ea98b0bacec157958a50f28b6b1d0839a66377
unknown <drmdfoureilu@gmail.com>
2018-07-27 16:37:33 -0500

    adding folders, base template and import files template, setting mongo db, requirements.txt established first specs

--- cont_project/settings.py
+++ cont_project/settings.py
@@ -74,10 +74,10 @@ WSGI_APPLICATION = 'cont_project.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+   'default': {
+      'ENGINE': 'djongo',
+      'NAME': 'cont_db',
+   }
 }
 
 
@@ -103,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-mx'
 
 TIME_ZONE = 'UTC'
 
@@ -118,3 +118,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
+LOGIN_REDIRECT_URL = '/'
+STATIC_ROOT=os.path.join(BASE_DIR,"www","static")
